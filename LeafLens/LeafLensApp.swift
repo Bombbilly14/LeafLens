45dd4b4a38280fcfa28ebf631b5bd8b5ce530d97
William German <115195062+Bombbilly14@users.noreply.github.com>
2025-04-24 12:29:29 -0700

    Initial Commit

---
+++ LeafLens/LeafLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafLensApp.swift
+//  LeafLens
+//
+//  Created by William German on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct LeafLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LeafLensTests/LeafLensTests.swift
@@ -0,0 +1,17 @@
+//
+//  LeafLensTests.swift
+//  LeafLensTests
+//
+//  Created by William German on 4/24/25.
+//
+
+import Testing
+@testable import LeafLens
+
+struct LeafLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
